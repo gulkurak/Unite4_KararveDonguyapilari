ac4d601ac13fc0357e4cf54172ad8942f43f114f
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-01-11 11:56:23 +0300

    karar ve dongu yapilari

---
+++ app/src/main/java/com/example/unite4_kararvedonguyapilari/uyg10Activity.java
@@ -0,0 +1,6 @@
+package com.example.unite4_kararvedonguyapilari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class uyg10Activity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/unite4_kararvedonguyapilari/uyg9Activity.java
@@ -0,0 +1,42 @@
+package com.example.unite4_kararvedonguyapilari;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class uyg9Activity extends AppCompatActivity {
+    EditText editTextSayilar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.uyg9_activity);
+        editTextSayilar = findViewById(R.id.txtSayilar);
+    }
+    public void CiftSayilar(View view){
+        editTextSayilar.setText("");
+        for (int i=0;i<15;i++){
+            if(i%2==0){
+                editTextSayilar.setText(editTextSayilar.getText()+""+i);
+            }
+        }
+
+        public void tekSayilar(View view){
+            editTextSayilar.setText("");
+            for (int i=0;i<=15;i++){
+                if (i%2!=0){
+                    editTextSayilar.setText(editTextSayilar.getText()+"\n"+i);
+                }
+            }
+        }
+        public void TumSayilar(View view){
+            editTextSayilar.setText("");
+            for (int i=0;i<=15;i++){
+                editTextSayilar.setText(editTextSayilar.getText()+"\n"+i);
+            }
+        }
+    }
+}
